122d7580709fc5aabd5716c093e6db1e29cff234
Jason Huang <jasonhuang20035@gmail.com>
2025-01-07 14:34:58 -0500

    fix: formatting, add db

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,29 +1,29 @@
 provider "aws" {
-    region = var.aws_region
-    profile = var.aws_profile
-    default_tags {
-        tags = {
-            app-id = "meetingbot"
-            app-purpose = "meeting bot"
-            environment = "${terraform.workspace == "default" ? "prod" : terraform.workspace}"
-            pii = "yes"
-        }
+  region  = var.aws_region
+  profile = var.aws_profile
+  default_tags {
+    tags = {
+      app-id      = "meetingbot"
+      app-purpose = "meeting bot"
+      environment = "${terraform.workspace == "default" ? "prod" : terraform.workspace}"
+      pii         = "yes"
     }
+  }
 }
 
 data "aws_availability_zones" "available" {}
 
 locals {
-    name = "meetingbot"
+  name = "meetingbot"
 
-    vpc_cidr = "10.0.0.0/16"
-    azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
 terraform {
-    backend "s3" {
-        encrypt        = true
-    }
+  backend "s3" {
+    encrypt = true
+  }
 }
 
 module "vpc" {
@@ -37,8 +37,8 @@ module "vpc" {
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
   public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
 
-#   enable_nat_gateway = true
-#   single_nat_gateway = true
+  #   enable_nat_gateway = true
+  #   single_nat_gateway = true
 }
 
 module "api_gateway" {
@@ -133,5 +133,82 @@ module "alb" {
         status_code  = "200"
       }
     }
+  }
+}
+
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = local.name
+
+  # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
+  engine               = "postgres"
+  engine_version       = "17"
+  family               = "postgres17" # DB parameter group
+  major_engine_version = "17"         # DB option group
+  instance_class       = "db.t4g.micro"
+
+  allocated_storage     = 10
+  max_allocated_storage = 100
+
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  db_name  = "postgres"
+  username = "postgres"
+  port     = 5432
+
+  # Setting manage_master_user_password_rotation to false after it
+  # has previously been set to true disables automatic rotation
+  # however using an initial value of false (default) does not disable
+  # automatic rotation and rotation will be handled by RDS.
+  # manage_master_user_password_rotation allows users to configure
+  # a non-default schedule and is not meant to disable rotation
+  # when initially creating / enabling the password management feature
+  manage_master_user_password_rotation              = true
+  master_user_password_rotate_immediately           = false
+  master_user_password_rotation_schedule_expression = "rate(15 days)"
+
+  multi_az               = true
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  create_monitoring_role                = true
+  monitoring_interval                   = 60
+  monitoring_role_name                  = "db-monitoring-role"
+  monitoring_role_use_name_prefix       = true
+  monitoring_role_description           = "Database monitoring role"
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
+    }
+  ]
+
+  tags = local.tags
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
+  }
+  cloudwatch_log_group_tags = {
+    "Sensitive" = "high"
   }
 }

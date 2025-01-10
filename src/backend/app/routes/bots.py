d4471c4cdfe7b16d9a11f1899f176764d6ce1d68
Sarah Simionescu <97263859+sarahsimionescu@users.noreply.github.com>
2025-01-10 00:18:31 -0500

    feat: return audio url when get bot (#60)
    complete MEE-97

--- src/backend/app/routes/bots.py
+++ src/backend/app/routes/bots.py
@@ -2,14 +2,17 @@ from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from app.models import Bot, MeetingInfo
 from app.models import BotCreate
-
+import boto3
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
-
+import dotenv
+import os
 from app.db import get_session
 
-router = APIRouter()
+dotenv.load_dotenv()
 
+router = APIRouter()
+client = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'), region_name=os.getenv('AWS_REGION'))
 
 @router.post("/create_bot", response_model=Bot)
 async def create_bot(data: BotCreate, session: AsyncSession = Depends(get_session)):
@@ -41,4 +44,16 @@ async def get_bot(bot_id: int, session: AsyncSession = Depends(get_session)):
     bot = result.first()
     if not bot:
         raise HTTPException(status_code=404, detail="Bot not found")
+    
+    key = bot.audio_s3_object_key
+
+    audio_recording_url = client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': os.getenv('AWS_BUCKET_NAME'), 'Key': key},
+        ExpiresIn=3600
+    )
+
+    bot.audio_recording_url = audio_recording_url # add the url to the bot object
+    del bot.audio_s3_object_key # remove the key from the bot object
+
     return bot

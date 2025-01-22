1fc7f2f7c39d583cab9d0ed5785046becb7b9b3e
Sarah Simionescu <97263859+sarahsimionescu@users.noreply.github.com>
2025-01-22 13:13:35 -0500

    feat: print statement at heartbeat interval (#76)
    towards MEE-137

--- src/bots/teams/src/index.ts
+++ src/bots/teams/src/index.ts
@@ -15,6 +15,8 @@ const organizerId = config.meeting_info.organizer_id;
 const tenantId = config.meeting_info.tenant_id;
 const displayName = config.bot_display_name;
 const callbackUrl = config.callback_url;
+const heartbeatInterval = config.heartbeat_interval;
+
 
 if (typeof meetingId !== "string") {
   throw new Error("Invalid meeting ID in config.json");
@@ -26,6 +28,8 @@ if (typeof meetingId !== "string") {
   throw new Error("Invalid display name in config.json");
 } else if (callbackUrl != null && typeof callbackUrl !== "string") {
   throw new Error("Invalid callback URL in config.json");
+} else if (typeof heartbeatInterval !== "number") {
+  throw new Error("Invalid heartbeat interval in config.json");
 }
 
 const url = `https://teams.microsoft.com/v2/?meetingjoin=true#/l/meetup-join/19:meeting_${meetingId}@thread.v2/0?context=%7b%22Tid%22%3a%22${tenantId}%22%2c%22Oid%22%3a%22${organizerId}%22%7d&anon=true`;
@@ -55,126 +59,139 @@ const leaveButtonSelector =
   'button[aria-label="Leave (Ctrl+Shift+H)"], button[aria-label="Leave (⌘+Shift+H)"]';
 
 (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await launch({
-    executablePath: puppeteer.executablePath(),
-    headless: "new",
-    // args: ["--use-fake-ui-for-media-stream"],
-    args: ["--no-sandbox"],
-  });
-
-  // Parse the URL
-  const urlObj = new URL(url);
-
-  // Override camera and microphone permissions
-  const context = browser.defaultBrowserContext();
-  context.clearPermissionOverrides();
-  context.overridePermissions(urlObj.origin, ["camera", "microphone"]);
-
-  // Open a new page
-  const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto(urlObj.href);
-
-  // Fill in the display name
-  await page
-    .locator(`[data-tid="prejoin-display-name-input"]`)
-    .fill(displayName ?? "Meeting Bot");
-
-  // Mute microphone before joining
-  await page.locator(`[data-tid="toggle-mute"]`).click();
-
-  // Join the meeting
-  await page.locator(`[data-tid="prejoin-join-button"]`).click();
-
-  // Listen for changes to the people in the meeting
-  page.locator(`aria-label="People"`).on("DOMSubtreeModified", async (e) => {
-    console.log("People changed");
-    console.log(e);
-  });
-
-  // Wait until join button is disabled or disappears
-  await page.waitForFunction(
-    (selector) => {
-      const joinButton = document.querySelector(selector);
-      return !joinButton || joinButton.hasAttribute("disabled");
-    },
-    {},
-    '[data-tid="prejoin-join-button"]'
-  );
-
-  // Check if we're in a waiting room by checking if the join button exists and is disabled
-  const joinButton = await page.$('[data-tid="prejoin-join-button"]');
-  const isWaitingRoom =
-    joinButton &&
-    (await joinButton.evaluate((button) => button.hasAttribute("disabled")));
-
-  let timeout = 30000;
-  if (isWaitingRoom) {
-    console.log("Joined waiting room");
-    timeout = 0; // wait indefinitely in the waiting room
-  }
-
-  // First wait for the leave button to appear (meaning we've joined the meeting)
-  await page.waitForSelector(leaveButtonSelector, {
-    timeout: timeout,
-  });
-  console.log("Successfully joined meeting");
-
-  // Get the stream
-  const stream = await getStream(page, { audio: true, video: true });
-
-  // Pipe the stream to a file
-  console.log("Recording...");
-  stream.pipe(file);
-
-  // Then wait for meeting to end by watching for the "Leave" button to disappear
-  await page.waitForFunction(
-    (selector) => !document.querySelector(selector),
-    {},
-    leaveButtonSelector
-  );
-  console.log("Meeting ended");
-
-  // Stop recording
-  await stream.destroy();
-  file.close();
-  console.log("Recording finished");
-
-  // Upload recording to S3
-  console.log("Uploading recording to S3...");
-  const fileContent = await fs.promises.readFile(recordingPath);
-  const uuid = crypto.randomUUID();
-  const key = `recordings/${uuid}-teams-recording.webm`;
+  const intervalId = setInterval(() => {
+    console.log(`[${new Date().toISOString()}] Bot is running...`);
+  }, heartbeatInterval); // Logs every heartbeatInterval milliseconds
 
   try {
-    await s3Client.send(
-      new PutObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: key,
-        Body: fileContent,
-        ContentType: "video/webm",
-      })
-    );
-    console.log(`Successfully uploaded recording to S3: ${key}`);
-
-    // Clean up local file
-    await fs.promises.unlink(recordingPath);
-  } catch (error) {
-    console.error("Error uploading to S3:", error);
-  }
-
-  console.log("Closing browser");
-  // Close the browser
-  await browser.close();
-  (await wss).close();
-
-  // call callback url if it exists
-  if (callbackUrl) {
-    await fetch(callbackUrl, {
-      method: "POST",
-      body: JSON.stringify({ key }),
+    // Launch the browser and open a new blank page
+    const browser = await launch({
+      executablePath: puppeteer.executablePath(),
+      headless: "new",
+      // args: ["--use-fake-ui-for-media-stream"],
+      args: ["--no-sandbox"],
     });
+
+    // Parse the URL
+    const urlObj = new URL(url);
+
+    // Override camera and microphone permissions
+    const context = browser.defaultBrowserContext();
+    context.clearPermissionOverrides();
+    context.overridePermissions(urlObj.origin, ["camera", "microphone"]);
+
+    // Open a new page
+    const page = await browser.newPage();
+
+    // Navigate the page to a URL
+    await page.goto(urlObj.href);
+
+    // Fill in the display name
+    await page
+      .locator(`[data-tid="prejoin-display-name-input"]`)
+      .fill(displayName ?? "Meeting Bot");
+
+    // Mute microphone before joining
+    await page.locator(`[data-tid="toggle-mute"]`).click();
+
+    // Join the meeting
+    await page.locator(`[data-tid="prejoin-join-button"]`).click();
+
+    // Listen for changes to the people in the meeting
+    page.locator(`aria-label="People"`).on("DOMSubtreeModified", async (e) => {
+      console.log("People changed");
+      console.log(e);
+    });
+
+    // Wait until join button is disabled or disappears
+    await page.waitForFunction(
+      (selector) => {
+        const joinButton = document.querySelector(selector);
+        return !joinButton || joinButton.hasAttribute("disabled");
+      },
+      {},
+      '[data-tid="prejoin-join-button"]'
+    );
+
+    // Check if we're in a waiting room by checking if the join button exists and is disabled
+    const joinButton = await page.$('[data-tid="prejoin-join-button"]');
+    const isWaitingRoom =
+      joinButton &&
+      (await joinButton.evaluate((button) => button.hasAttribute("disabled")));
+
+    let timeout = 30000;
+    if (isWaitingRoom) {
+      console.log("Joined waiting room");
+      timeout = 0; // wait indefinitely in the waiting room
+    }
+
+    // First wait for the leave button to appear (meaning we've joined the meeting)
+    await page.waitForSelector(leaveButtonSelector, {
+      timeout: timeout,
+    });
+    console.log("Successfully joined meeting");
+
+    // Get the stream
+    const stream = await getStream(page, { audio: true, video: true });
+
+    // Pipe the stream to a file
+    console.log("Recording...");
+    stream.pipe(file);
+
+    // Then wait for meeting to end by watching for the "Leave" button to disappear
+    await page.waitForFunction(
+      (selector) => !document.querySelector(selector),
+      {},
+      leaveButtonSelector
+    );
+    console.log("Meeting ended");
+
+    // Stop recording
+    await stream.destroy();
+    file.close();
+    console.log("Recording finished");
+
+    // Upload recording to S3
+    console.log("Uploading recording to S3...");
+    const fileContent = await fs.promises.readFile(recordingPath);
+    const uuid = crypto.randomUUID();
+    const key = `recordings/${uuid}-teams-recording.webm`;
+
+    try {
+      await s3Client.send(
+        new PutObjectCommand({
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: key,
+          Body: fileContent,
+          ContentType: "video/webm",
+        })
+      );
+      console.log(`Successfully uploaded recording to S3: ${key}`);
+
+      // Clean up local file
+      await fs.promises.unlink(recordingPath);
+    } catch (error) {
+      console.error("Error uploading to S3:", error);
+    }
+
+    console.log("Closing browser");
+    // Close the browser
+    await browser.close();
+    (await wss).close();
+
+    // call callback url if it exists
+    if (callbackUrl) {
+      await fetch(callbackUrl, {
+        method: "POST",
+        body: JSON.stringify({ key }),
+      });
+    }
+
+    // Clean up interval before exiting
+    clearInterval(intervalId);
+  } catch (error) {
+    // Clean up interval if there's an error
+    clearInterval(intervalId);
+    throw error;
   }
 })();

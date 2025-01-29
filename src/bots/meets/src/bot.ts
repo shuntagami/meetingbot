f16c7473402c5acfef8ce908f0433c024c231946
Alex Eckardt <50144777+alexeckardt@users.noreply.github.com>
2025-01-29 17:02:21 -0500

    feat: meets-upload-s3 (#52)
    completes MEE-67

---
+++ src/bots/meets/src/bot.ts
@@ -0,0 +1,178 @@
+import { chromium } from 'playwright-extra';
+import { Browser, Page } from 'playwright';
+import { saveVideo, PageVideoCapture } from 'playwright-video';
+import { CaptureOptions } from 'playwright-video/build/PageVideoCapture';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import fs from 'fs-extra';
+
+// Use Stealth
+const stealthPlugin = StealthPlugin();
+stealthPlugin.enabledEvasions.delete('iframe.contentWindow');
+stealthPlugin.enabledEvasions.delete('media.codecs');
+chromium.use(stealthPlugin);
+
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+
+const enterNameField = 'input[type="text"][aria-label="Your name"]';
+const askToJoinButton = '//button[.//span[text()="Ask to join"]]';
+const leaveButton = `//button[@aria-label="Leave call"]`
+const peopleButton = `//button[@aria-label="People"]`
+const onePersonRemainingField = '//span[.//div[text()="Contributors"]]//div[text()="1"]'
+
+export class MeetingBot {
+
+    browserArgs: string[];
+    meetingURL: string;
+    readonly settings: { [key: string]: any }
+
+    // Ssves
+    browser: Browser;
+    page: Page;
+    recorder: PageVideoCapture | undefined;
+
+
+    //
+    //
+    //
+    //
+    constructor(meetingURL, botSettings: { [key: string]: any }) {
+
+        this.browserArgs = [
+            '--incognito',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-features=IsolateOrigins,site-per-process',
+            '--disable-infobars',
+            "--use-fake-device-for-media-stream",
+            ...(botSettings?.additionalBrowserSettings ?? [])
+        ]
+
+        // Pull
+        const { name, viewport } = botSettings
+        this.settings = botSettings; //store all anyways
+
+        // Set
+        this.meetingURL = meetingURL;
+    }
+
+    //
+    //
+    //
+    //
+    async joinMeeting() {
+
+
+        // Launch the browser and open a new blank page
+        this.browser = await chromium.launch({
+            headless: false,
+            args: this.browserArgs
+        });
+
+        // Define Default View Port Dimensions
+        const vp = this.settings.viewport || { width: 1280, height: 720 };
+
+        // Create Browser Context
+        const context = await this.browser.newContext({
+            permissions: ['camera', 'microphone'],
+            userAgent: userAgent,
+            viewport: vp,
+        }
+        );
+
+        // Create Page, Go to
+        this.page = await context.newPage();
+        await this.page.goto(this.meetingURL, { waitUntil: 'networkidle' });
+
+        const name = this.settings.name || 'MeetingBot';
+
+        console.log('Waiting for the input field to be visible...');
+        await this.page.waitForSelector(enterNameField);
+
+        console.log('Waiting for 1 seconds...');
+        await this.page.waitForTimeout(1000);
+
+        console.log('Filling the input field with the name...');
+        await this.page.fill(enterNameField, name);
+
+        console.log('Waiting for the "Ask to join" button...');
+        await this.page.waitForSelector(askToJoinButton, { timeout: 60000 });
+
+        console.log('Clicking the "Ask to join" button...');
+        await this.page.click(askToJoinButton);
+
+        //Should Exit after 1 Minute
+        console.log('Awaiting Entry ....')
+        await this.page.waitForSelector(leaveButton, { timeout: 60000 })
+
+        console.log('Joined Call.')
+        return 0;
+    }
+
+    async startRecording() {
+
+        // Set Default Recording Options
+        const recordingOptions: CaptureOptions = this.settings.recordingOptions ||
+        {
+            followPopups: false,
+            fps: 25,
+        }
+
+        // Save the Recorder
+        this.recorder = await saveVideo(this.page, this.settings.recordingPath, recordingOptions);
+        return 0;
+    }
+
+    async stopRecording() {
+
+        // Error case
+        if (!this.recorder) {
+            console.log('Cannot stop recording as recording has not been started.')
+            return 1;
+        }
+
+        // Stop the Recording & Drop Reference
+        await this.recorder.stop();
+        this.recorder = undefined;
+
+        return 0;
+    }
+
+
+    async meetingActions() {
+
+        // Meeting Join Actions
+        console.log('Clicking People Button...')
+        await this.page.waitForSelector(peopleButton)
+        await this.page.click(peopleButton);
+
+        // Start Recording, Yes by default
+        if (this.settings.recordMeeting || true)
+            this.startRecording();
+
+        // Define Exit Condition(s)
+        const onlyMeInMeeting = this.page.locator(onePersonRemainingField).waitFor()
+        // const exitCondition2 = this.page.waitForFunction(() => document.title.includes('Success'));
+
+        // Chill Until an Exit Condition is met
+        const result = await Promise.race([onlyMeInMeeting, ]);
+
+        // Exit
+        await this.leaveMeeting();
+        return 0;
+    }
+
+    async leaveMeeting() {
+
+        // Ensure
+        this.stopRecording();
+
+        // Try and Find the leave button, press. Otherwise, just delete the browser.
+        await this.page.click(leaveButton);
+        console.log('Left Call.')
+
+        await this.browser.close();
+        console.log('Closed Browser.')
+
+        return 0;
+    }
+}
--- src/bots/meets/src/index.ts
+++ src/bots/meets/src/index.ts
@@ -1,106 +1,94 @@
-import { chromium } from 'playwright-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-import { saveVideo } from 'playwright-video';
-import fs from 'fs-extra';
-import { CaptureOptions } from 'playwright-video/build/PageVideoCapture';
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { MeetingBot } from "./bot";
+import fs, { readFileSync } from "fs";
+import dotenv from 'dotenv';
+import { setTimeout } from 'timers/promises';
+import crypto from 'crypto';
+import path from 'path';
 
-const stealthPlugin = StealthPlugin();
-stealthPlugin.enabledEvasions.delete('iframe.contentWindow');
-stealthPlugin.enabledEvasions.delete('media.codecs');
-chromium.use(stealthPlugin);
+dotenv.config()
 
-const url = "https://meet.google.com/tqx-axzw-ore";
+const main = (async () => {
 
-// const file = fs.createWriteStream(__dirname + "/test.webm");
+  // =======================================================
+  //                TODO Define via API args
+  //
+  //
+  const url = "https://meet.google.com/uoa-ewak-iva";
+  const recordingPath = './recording.mp4'
+  const botSettings = { name: 'Alex\'s Bot', recordingPath }
+  //
+  //
+  // =======================================================
 
-(async () => {
+  if (
+    !process.env.AWS_ACCESS_KEY_ID ||
+    !process.env.AWS_SECRET_ACCESS_KEY ||
+    !process.env.AWS_BUCKET_NAME ||
+    !process.env.AWS_REGION
+  ) {
+    throw new Error("Missing environment variables");
+  }
 
-  const browserArgs = [
-    '--incognito',
-    '--disable-features=IsolateOrigins,site-per-process',
-    '--disable-infobars',
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    "--use-fake-device-for-media-stream"
-  ]
-
-  // Launch the browser and open a new blank page
-  const browser = await chromium.launch({
-    headless: false,
-    args: browserArgs
+  // Initialize S3 client
+  const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
   });
 
+  // Create the bot with settings
+  const bot = new MeetingBot(url, botSettings);
 
-  // Create Browser Context
-  const context = await browser.newContext({
-    permissions: ['camera', 'microphone'],
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    viewport: { width: 1280, height: 720 },
+  //  Core
+
+  //Join meeting
+  const join = await bot.joinMeeting();
+
+  // Perform meeting actions if successfull
+  if (join == 1) {
+    console.log('The bot could not join the meeting.')
+    return 1;
   }
-  );
 
-  const fullName = 'Meeting Bot';
-
-  // Create Page
-  const page = await context.newPage();
-
-  console.log('Navigating to Google Meet URL...');
-  await page.goto(url, { waitUntil: 'networkidle' });
-
-  console.log('Waiting for the input field to be visible...');
-  await page.waitForSelector('input[type="text"][aria-label="Your name"]');
-
-  console.log('Waiting for 1 seconds...');
-  await page.waitForTimeout(1000);
-
-  console.log('Filling the input field with the name...');
-  await page.fill('input[type="text"][aria-label="Your name"]', fullName);
-
-  console.log('Waiting for the "Ask to join" button...');
-  await page.waitForSelector('//button[.//span[text()="Ask to join"]]', { timeout: 60000 });
-
-  console.log('Clicking the "Ask to join" button...');
-  await page.click('//button[.//span[text()="Ask to join"]]');
-
-  //
-  const leaveButton = `//button[@aria-label="Leave call"]`
-  const peopleButton = `//button[@aria-label="People"]`
+  // Bot Meeting Actions  
+  await bot.meetingActions();
 
 
-  //Should Exit after 1 Minute
-  console.log('Awaiting Entry ....')
-  await page.waitForSelector(leaveButton, { timeout: 60000 })
-
-  console.log('Joined Call.')
-  console.log('Starting Recording ....')
+  // Browser is now closed.
 
 
-  console.log('Finding People Button..')
-  await page.waitForSelector(peopleButton)
-  await page.click(peopleButton);
-  console.log('Clicked People Button.')
+  // Upload recording to S3
+  console.log("Uploading recording to S3...");
 
+  // 1 second delay
+  await setTimeout(5000);
 
-  //Start Recording
-  const recordingOptions: CaptureOptions = {
-    followPopups: false,
-    fps: 25,
+  const filePath = path.resolve(__dirname, "recording.mp4");
+
+  const fileContent = await readFileSync(recordingPath);
+  const uuid = crypto.randomUUID();
+  const key = `recordings/${uuid}-meets-recording.mp4`;
+
+  try {
+
+    const commandObjects = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key,
+      Body: fileContent,
+      ContentType: 'video/mp4',
+    };
+
+    const putCommand = new PutObjectCommand(commandObjects);
+    await s3Client.send(putCommand);
+    console.log(`Successfully uploaded recording to S3: ${key}`);
+
+    // Clean up local file
+    await fs.promises.unlink(recordingPath);
+  } catch (error) {
+    console.error("Error uploading to S3:", error);
   }
-  const recorder = await saveVideo(page, './test.mp4', recordingOptions);
-
-  // Detect All but Me Person Leave
-  await page.locator('//span[.//div[text()="Contributors"]]//div[text()="1"]').waitFor()
-  console.log('Detected 1 Person in the call -- exiting ...');
-
-  // End Recording
-  await recorder.stop();
-
-  // Leave Meeting
-  // Close Browser
-  await page.click(leaveButton);
-  console.log('Left Call.')
-
-  await browser.close();
-  console.log('Closed Browser.')
-
-})();
+})
+main();

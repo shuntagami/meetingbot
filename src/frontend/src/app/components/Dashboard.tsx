e6428fe76625e9e97e31ea297f3ca216500b0e7d
Owen Gretzinger <owengretzinger@gmail.com>
2025-03-27 09:34:27 -0400

    fix: fix dashboard layout (#235)

--- src/frontend/src/app/components/Dashboard.tsx
+++ src/frontend/src/app/components/Dashboard.tsx
@@ -36,67 +36,61 @@ export default function Dashboard() {
       </div>
       <div>
         <div className="grid grid-cols-1 gap-6 lg:h-[48rem] lg:grid-cols-3">
-          <div className="h-full lg:min-h-0">
-            <DashboardCard
-              title="Active Bots"
-              className="h-full min-h-56"
-              content={
-                activeBotCountLoading ? (
-                  <Skeleton className="h-10 w-10" />
-                ) : activeBotCountError ? (
-                  <div className="text-4xl font-bold">
-                    <ErrorAlert errorMessage={activeBotCountError.message} />
-                  </div>
-                ) : (
-                  <div className="text-4xl font-bold">
-                    {activeBotCount?.count}
-                  </div>
-                )
-              }
-              icon={<Bot />}
-              link={{
-                type: "INTERNAL",
-                url: "/bots",
-                text: "View Bots",
-              }}
-            />
-          </div>
-          <div className="h-full lg:min-h-0">
-            <DashboardCard
-              title="Active Keys"
-              className="h-full min-h-56"
-              content={
-                keyCountLoading ? (
-                  <Skeleton className="h-10 w-10" />
-                ) : keyCountError ? (
-                  <div className="text-4xl font-bold">
-                    <ErrorAlert errorMessage={keyCountError.message} />
-                  </div>
-                ) : (
-                  <div className="text-4xl font-bold">{keyCount?.count}</div>
-                )
-              }
-              icon={<Key />}
-              link={{
-                type: "INTERNAL",
-                url: "/keys",
-                text: "View Keys",
-              }}
-            />
-          </div>
-          <div className="h-full lg:min-h-0">
-            <DashboardCard
-              title="View our Docs"
-              className="h-full min-h-56"
-              content="To learn more about how to create bots, pull meeting recordings, pull transcriptions and more, view our Documentation!"
-              icon={<File className="text-slate-500" />}
-              link={{
-                type: "EXTERNAL",
-                url: `${env.NEXT_PUBLIC_BACKEND_URL}/docs`,
-                text: "View Documentation",
-              }}
-            />
-          </div>
+          <DashboardCard
+            title="Active Bots"
+            className="h-full min-h-56"
+            content={
+              activeBotCountLoading ? (
+                <Skeleton className="h-10 w-10" />
+              ) : activeBotCountError ? (
+                <div className="text-4xl font-bold">
+                  <ErrorAlert errorMessage={activeBotCountError.message} />
+                </div>
+              ) : (
+                <div className="text-4xl font-bold">
+                  {activeBotCount?.count}
+                </div>
+              )
+            }
+            icon={<Bot />}
+            link={{
+              type: "INTERNAL",
+              url: "/bots",
+              text: "View Bots",
+            }}
+          />
+          <DashboardCard
+            title="Active Keys"
+            className="h-full min-h-56"
+            content={
+              keyCountLoading ? (
+                <Skeleton className="h-10 w-10" />
+              ) : keyCountError ? (
+                <div className="text-4xl font-bold">
+                  <ErrorAlert errorMessage={keyCountError.message} />
+                </div>
+              ) : (
+                <div className="text-4xl font-bold">{keyCount?.count}</div>
+              )
+            }
+            icon={<Key />}
+            link={{
+              type: "INTERNAL",
+              url: "/keys",
+              text: "View Keys",
+            }}
+          />
+          <DashboardCard
+            title="View our Docs"
+            className="h-full min-h-56"
+            content="To learn more about how to create bots, pull meeting recordings, pull transcriptions and more, view our Documentation!"
+            icon={<File className="text-slate-500" />}
+            link={{
+              type: "EXTERNAL",
+              url: `${env.NEXT_PUBLIC_BACKEND_URL}/docs`,
+              text: "View Documentation",
+            }}
+          />
           <div className="h-[30rem] min-h-0 lg:row-span-2 lg:h-full">
             <CommunityCard className="h-full" />
           </div>

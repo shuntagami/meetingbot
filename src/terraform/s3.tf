ea8513fa7b221ab4743937456c1e83979061cb8e
Jason Huang <jasonhuang20035@gmail.com>
2025-04-03 20:48:19 -0400

    feat: add random string to s3 bucket (#252)

--- src/terraform/s3.tf
+++ src/terraform/s3.tf
@@ -1,3 +1,12 @@
+resource "random_string" "this" {
+  length  = 8
+
+  numeric = true
+  upper = false
+  lower = false
+  special = false
+}
+
 resource "aws_s3_bucket" "this" {
-  bucket = "${local.name}-bot-data"
+  bucket = "${local.name}-${random_string.this.result}-bot-data"
 }
